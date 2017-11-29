5d76ea60efae1a53bc19b917f40f1e40f46a2640
MarianGP <mgarciapeters@gmail.com>
2017-11-29 11:10:37 +0100

    intermediate

---
+++ script.js
@@ -0,0 +1,21 @@
+
+team = ["Bill ", "Mary ", "Mike ", "Chris ", "Anne "];
+document.write(team + "<br>");
+
+document.write(team.sort() + "<br>");
+
+console.log(team.pop());
+document.write(team + "<br>");
+
+team.push("Joseph");
+console.log(team + "<br>");
+
+var fruits = "mango/cherries/kiwi/grapes/pear/peach/orange/lemon";
+var SingleFruits = new Array ();
+SingleFruits = fruits.split("/");
+document.write(SingleFruits[0] +  "<br>" + SingleFruits[1] + "<br>" + SingleFruits[2]);
+
+
+
+
+// + "<br>"
